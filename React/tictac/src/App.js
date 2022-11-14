be7550bffa670cb68b821a2d2c60bb5c7066de68
Emgula96 <ethan_gula@yahoo.com>
2022-11-14 11:37:09 -0500

    tic tac

---
+++ React/tictac/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TitTacToe from "./TitTacToe";
+
+function App() {
+  return (
+    <div className="App">
+      <TitTacToe />
+    </div>
+  );
+}
+
+export default App;
