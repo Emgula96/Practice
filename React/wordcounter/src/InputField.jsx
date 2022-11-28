4b694a6d91a48e4b0d3c1571a8c0387f8b0959ef
Emgula96 <ethan_gula@yahoo.com>
2022-11-28 12:12:04 -0500

    Update InputField.jsx

--- React/wordcounter/src/InputField.jsx
+++ React/wordcounter/src/InputField.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 import DisplayArea from './DisplayArea'
 const InputField = () => {
 
+    const [word, setWord] = useState('')
+    let wordToCount = word
+    console.log(word)
+
+    const counterFunction = (word) => {
+        
+    }
     
-    let count = { }
     return (
     <>
-    <input classname='string-input' placeholder='Enter your word'></input>
+            <input className='string-input' onChange={(e) => setWord(e.target.value)} placeholder='Enter your word'></input>
             <button className='Enter-btn'>Enter</button>
-            <DisplayArea count={count} />
+            <DisplayArea count={wordToCount} />
     </>
   )
 }
